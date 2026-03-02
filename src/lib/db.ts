b4010f5918b57bc5d0eefb84a7a5f5b997e79541
robert <robert.walker6580@gmail.com>
2026-03-01 17:16:41 -0800

    01: project setup, authentication & database

---
+++ src/lib/db.ts
@@ -0,0 +1,22 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from '@/generated/prisma/client';
+import { env } from '@/lib/env';
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
